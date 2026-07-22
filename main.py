88391fb095ba1a3666a1b544ac39810e5ff4cc12
Vitoria Favoreto <vitoria_favoreto@hotmail.com>
2026-07-22 21:02:14 +0000

    Cria estrutura inicial dos testes no main

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-
+import data
 class TestUrbanRoutes:
+    def test_set_route(self):
+        # Adicionar em S8
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        pass
